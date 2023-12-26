83fa8dfb3aef995f4cefcf0deb8c6a1d7d776722
amir3672@gmail.com <amir3672@gmail.com>
2023-12-26 12:53:54 +0330

    update PostgresConnectionManager.php add schema

--- app/PostgresConnectionManager.php
+++ app/PostgresConnectionManager.php
@@ -10,7 +10,6 @@ class PostgresConnectionManager
     public Channel $channel;
     private array $connectionConfig;
     private CliLogger $cliPrinter;
-
     private static int $connectionCount = 0 ;
 
 
@@ -32,22 +31,19 @@ class PostgresConnectionManager
 
     public function make(): PDO
     {
-        $pdo =  new PDO(
+        return new PDO(
             "pgsql:host={$this->connectionConfig['host']};port={$this->connectionConfig['port']};dbname={$this->connectionConfig['dbname']};user={$this->connectionConfig['user']};password={$this->connectionConfig['password']}",
             $this->connectionConfig['user'],
             $this->connectionConfig['password']
         );
-        return $pdo;
     }
 
     public function saveLinkStatics( $requestData = [] , string $tableName = 'links_statics'): void
     {
         if (static::$connectionCount === 0)
             $this->initializeConnections();
-
         $pdo = $this->channel->pop();
         try {
-
             $stmt = $pdo->prepare("INSERT INTO " . DATABASE_SCHEMA . '.' . $tableName . " (os, os_version, browser, browser_version, client_ip, base_url, url_path, full_url, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
             $stmt->bindParam(1, $requestData['os']);
             $stmt->bindParam(2, $requestData['os_version']);
