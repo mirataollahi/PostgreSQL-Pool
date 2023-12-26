e9576fe0dccf08a43ea602abacbc8cd3fc7cd9b1
amir3672@gmail.com <amir3672@gmail.com>
2023-12-26 12:59:34 +0330

    update PostgresConnectionManager.php add schema

--- start.php
+++ start.php
@@ -22,8 +22,8 @@ const DATABASE_NAME = 'search';
 const DATABASE_CHARSET = 'utf8';
 const DATABASE_USERNAME = 'postgres';
 const DATABASE_PASSWORD = 'password';
-const DATABASE_SCHEMA = 'amir';
-const POSTGRES_POOL_SIZE = 64;
+const DATABASE_SCHEMA = 'public';
+const POSTGRES_POOL_SIZE = 16;
 const LINKS_TABLE = 'links_statics';
 
 const IS_PRODUCTION = false;
